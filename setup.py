583f7584e02d90566129819bf06a8f1545bf51ea
MAHDI ALL <mehdiwinneri01@gmail.com>
2026-01-30 11:40:41 -0500

    version incremented (minor)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="MEDiml",
-    version="0.9.8",
+    version="0.9.9",
     author="MEDomics consortium",
     author_email="medomics.info@gmail.com",
     description="Python Open-source package for medical images processing and radiomic features extraction",
