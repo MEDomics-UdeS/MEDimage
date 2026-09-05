7093c9124e245d24708c1934bac879af22eca802
mahdiall99 <mehdiwinneri01@gmail.com>
2026-09-05 13:28:35 -0400

    version upgrade

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="MEDiml",
-    version="0.15.3",
+    version="0.15.4",
     author="MEDomics consortium",
     author_email="medomics.info@gmail.com",
     description="Python Open-source package for medical images processing and radiomic features extraction",
